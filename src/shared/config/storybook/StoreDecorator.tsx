1772d468679f6684ea6ee44476f7e412a932a35c
Igor.bezmestin <igor-rock@list.ru>
2023-09-27 13:18:44 +0300

    fix: fixed decorators

--- src/shared/config/storybook/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator.tsx
@@ -1,20 +1,11 @@
 import { StoryFn } from '@storybook/react';
 import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
-import { profileReducer } from 'entities/Profile';
-import { loginReducer } from 'features/AuthByUsername/model/slice/loginSlice';
-import { ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
-
-const defaultAsyncReducers: ReducersList = {
-    loginForm: loginReducer,
-    profile: profileReducer,
-};
 
 export const StoreDecorator =
-    (initialState: DeepPartial<StateSchema>, asyncReducers?: ReducersList) => (Story: StoryFn) =>
+    (initialState: DeepPartial<StateSchema>) => (Story: StoryFn) =>
         (
             <StoreProvider
                 initialState={initialState as StateSchema}
-                asyncReducers={{ ...defaultAsyncReducers, ...asyncReducers }}
             >
                 <Story />
             </StoreProvider>
